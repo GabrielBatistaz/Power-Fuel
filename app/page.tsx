7d2985d0a52b6b43fae5c87ba4bd1e71e68a8ce7
GabrielBatistaz <gabrielb99u@gmail.com>
2024-05-20 17:43:27 -0300

    teste hospedagem

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
-import Image from 'next/image'
 import Container from './components/container';
 import Homebanner from './components/nav/homebanner';
 import { products } from '@/utils/products';
-import { cuttext } from '@/utils/cuttext';
 import Productsheet from './components/products/Productsheet';
 
 
