406c11318e8a56a7b24ec1cae96a6a4f01d4b43a
GabrielBatistaz <gabrielb99u@gmail.com>
2024-05-20 17:49:23 -0300

    hosp

--- app/components/products/Productsheet.tsx
+++ app/components/products/Productsheet.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { cuttext } from "@/utils/cuttext";
 import { Typeprice } from "@/utils/typeprice";
 import { Rating } from "@mui/material";
@@ -19,20 +17,23 @@ const Productsheet: React.FC<ProductsheetProps> = ({ data }) => {
     return (
         <div onClick={() => router.push(`/product/${data.id}`)} className="col-span-1 cursor-pointer border-[1.2px] border-slate-200 bg-slate-50 rounded-sm p-2 transition hover:scale-105 text-center text-sm">
             <div className="flex flex-col items-center w-full gap-1">
-                <div className="aspect-square overflow-hidden relative w-full">
-                    <Image
-                        fill
-                        src={data.images[0].image}
-                        alt={data.nome}
-                        className="w-full h-full object-contain"
-                    />
-                </div>
+                {data.images.map((image: any, index: number) => (
+                    <div key={index} className="aspect-square overflow-hidden relative w-full">
+                        <Image
+                            fill
+                            src={image.image}
+                            alt={data.nome}
+                            className="w-full h-full object-contain"
+                        />
+                    </div>
+                ))}
                 <div className="mt-4">{cuttext(data.nome)}</div>
                 <div><Rating value={productRating} readOnly /></div>
                 <div>{data.reviews.length} reviews</div>
                 <div className="font-bold">{Typeprice(data.preco)}</div>
             </div>
-        </div>);
+        </div>
+    );
 }
 
 export default Productsheet;
